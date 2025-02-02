24a8b5ab90725b6f62e75aa6b223020966f3953f
Paul Habfast <paul@thecakeisalive.com>
2025-02-02 16:17:38 +0100

    added middleware for CORS for local development

---
+++ backend/museumplanner/middlewares.py
@@ -0,0 +1,32 @@
+from urllib.parse import urlparse
+
+from django.http import HttpRequest, HttpResponse
+
+
+def cors_development_middleware(get_response):
+    """
+    A middleware that sets CORS response headers for local development
+    """
+
+    def middleware(request: HttpRequest):
+        if request.method == "OPTIONS":
+            response = HttpResponse()
+        else:
+            response = get_response(request)
+
+        if "Origin" in request.headers:
+            origin = urlparse(request.headers["Origin"])
+            if origin.hostname.endswith("localhost") or origin.hostname == "host.docker.internal":
+                response.headers["Access-Control-Allow-Origin"] = f"{origin.scheme}://{origin.hostname}:{origin.port}"
+                response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+                response.headers[
+                    "Access-Control-Allow-Headers"
+                ] = "Content-Type, Authorization, X-Requested-With, X-CSRFToken"
+                response.headers["Access-Control-Allow-Credentials"] = "true"
+                # comma-separated list of headers that is accessible to the frontend code
+                # response.headers["Access-Control-Expose-Headers"] = ""
+                response.headers["Vary"] = "Origin"
+
+        return response
+
+    return middleware
--- backend/museumplanner/settings.py
+++ backend/museumplanner/settings.py
@@ -37,6 +37,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "museumplanner.middlewares.cors_development_middleware",
 ]
 
 ROOT_URLCONF = "museumplanner.urls"
